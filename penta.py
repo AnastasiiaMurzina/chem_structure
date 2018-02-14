b6cb4c8252dac70ae4da46c93cb4bb29b9d83490
AnastasiiaMurzina <hloshkin@mail.ru>
2018-02-14 13:58:24 +0300

    Update penta.py
    Prepare dodecahedron to rotate. All must be changed zenith angle to x = _ * sin(v_angle), z = r * cos(v_angle). Show_by_angles is ready for thinking in term of Rodrigues' rotation formula.

--- penta.py
+++ penta.py
@@ -1,4 +1,4 @@
-from math import acos, sin, pi, cos
+from math import acos, sin, pi, cos, atan, asin
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from itertools import combinations
@@ -8,7 +8,7 @@ r = 1
 a = 4 * r * (10 + 22 / 5 ** 0.5) ** (-0.5)  # length of the side
 d = a * sin(3 * pi / 10) / sin(2 * pi / 5)  # distance between the center and the pentagon angle
 h = d * sin(3 * pi / 10)  # hight to the middle of the side
-angle = acos(-5 ** (-0.5))  # between two surfaces
+angle = acos(- 5 ** (-0.5))  # between two surfaces
 center_up = np.array([0, 0, 1])
 center_down = np.array([0, 0, -1])
 center = np.array([0, 0, 0])
@@ -38,18 +38,11 @@ def get_down_surface_centers(point_from):
         initial_angle += 2 * pi / 5
     return centers
 
-def show_points(array_points):
+def show_points(points):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    for i in array_points:
+    for i in points:
         ax.scatter(i[0], i[1], i[2])
-    plt.show()
-
-def show_points_by_angles(center, array_angles):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    for item, i in array_angles.items():
-        ax.scatter(center[0]+cos(i[0])*sin(i[1]), center[1] + sin(i[0])*sin(i[1]), center[2] + cos(i[1]))
     plt.show()
 
 def get_penta_points(point_center):
@@ -64,6 +57,48 @@ def get_dictionary_coordinates(penta_points):
     for i, j in enumerate(penta_points):
         dictionary.update({i+1: j})
     return dictionary
+
+def section_num_by_coords(center_point, penta_point):
+    dictionary = get_dictionary_coordinates(get_penta_points(center_point))
+    for key, item in dictionary.items():
+        diff = np.array(item) - np.array(penta_point)
+        diff = np.linalg.norm(diff)
+        if diff < d:
+            return key
+
+def horizontal_rotate_points(points, alpha):
+    for key, item in points.items():
+        x, y, z = item
+        x, y = [x * cos(alpha) - y * sin(alpha), x*sin(alpha)+y*cos(alpha)]
+        points[key] = np.array([x,y,z])
+    return points
+
+#########################angles###########################
+def show_points_by_angles(center, angles, labels=False):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    if labels:
+        for item, i in angles.items():
+            ax.scatter(center[0]+cos(i[0])*sin(i[1]), center[1] + sin(i[0])*sin(i[1]), center[2] + cos(i[1]))
+            ax.text(center[0]+cos(i[0])*sin(i[1]), center[1] + sin(i[0])*sin(i[1]), center[2] + cos(i[1]), item)
+    else:
+        for item, i in angles.items():
+            ax.scatter(center[0] + cos(i[0]) * cos(i[1]), center[1] + sin(i[0]) * cos(i[1]), center[2] + sin(i[1]))
+
+    plt.show()
+
+def get_penta_angles():
+    angles = [np.array([0,pi/2])]
+    init_angle = 0
+    for i in range(5):
+        init_angle += (2 * pi / 5) % (2 * pi)
+        angles.append(np.array([init_angle, asin(sin(angle)/2)]))
+    # print(sin(angle)/2)
+    angles.append(np.array([0, -pi/2]))
+    for i in range(5):
+        init_angle += (2 * pi / 5) % (2 * pi)
+        angles.append(np.array([init_angle, -asin(sin(angle)/2)]))
+    return angles
 
 def get_dictionary_angles(center_point, penta_points):
     dictionary = {}
@@ -80,32 +115,65 @@ def get_dictionary_angles(center_point, penta_points):
     return dictionary
 
 # BUG HERE! BE careful
-def section_num_by_coords(center_point, penta_point):
-    dictionary = get_dictionary_coordinates(get_penta_points(center_point))
-    for key, item in dictionary.items():
-        diff = np.array(item) - np.array(penta_point)
-        diff = np.linalg.norm(diff)
-        if diff < d:
-            return key
+
+def rotate(angles, alpha, betta):
+    for key, item in angles.items():
+        angles[key] = np.array([(angles[key][0]+alpha)%(2*pi), angles[key][1]+betta if abs(angles[key][1]+betta) < pi/2 else (pi - angles[key][1]-betta)])
+    return angles
+
+
+def horizontal_rotate_angles(angles, alpha):
+    for key, item in angles.items():
+        pass
+        # angles[key] = np.array([angles[key][0]*cos(alpha)])
+
+def vertical_rotate_angles(angles, betta):
+    for key, item in angles.items():
+        if item[0]>pi:
+            v_angle = angles[key][1] + betta
+        else:
+            v_angle = angles[key][1] - betta
+        if abs(v_angle) > pi/2:
+            if v_angle>0:
+                angles[key][1] = pi - v_angle
+            else:
+                angles[key][1] = - pi - v_angle
+            angles[key][0] = (angles[key][0] + pi) % (2 * pi)
+    return angles
 
 ########################################################
 
 if __name__ == '__main__':
     my_arr = get_penta_points(center)
     darr = (get_dictionary_coordinates(my_arr))
-    show_points(my_arr)
+    show_points([item for key,item in darr.items()])
+    # darr = horizontal_rotate_points(darr, pi/2)
+
+    # show_points([item for key,item in darr.items()])
+
+    a2 = get_penta_angles()
+    ann_arr = get_dictionary_coordinates(a2)
+    print(ann_arr)
+    ann_arr = vertical_rotate_angles(ann_arr, pi/6)
+    show_points_by_angles(np.array([0,0,0]), ann_arr, labels=True)
+
+    # ann_arr = rotate(ann_arr, 0, pi/12)
+    # print(ann_arr)
+    # show_points_by_angles(np.array([0,0,0]), ann_arr, labels=True)
+
     # fig = plt.figure()
     # ax = fig.add_subplot(111, projection='3d')
-    # for key, item in darr.items():
-    #     ax.scatter(item[0], item[1], item[2])
-    #     ax.text(item[0], item[1], item[2], key)
+    # for i in a2:
+    #     p = np.array([cos(i[0])*cos(i[1]), sin(i[0])*cos(i[1]), sin(i[1])])
+    #     ax.scatter(cos(i[0])*cos(i[1]), sin(i[0])*cos(i[1]), sin(i[1]))
+
+
     # plt.show()
-    # print(darr)
 
 #######################FALL TEST###############################################
-    zero = np.array([0, 0, 0])
-    for key, item in darr.items():
-        print(section_num_by_coords(zero, item), section_num_by_coords(item, zero))
+    # zero = np.array([0, 0, 0])
+    # for key, item in darr.items():
+    #     print(section_num_by_coords(zero, item), section_num_by_coords(item, zero))
 #######################################################################################
     # print(section_num_by_coords(np.array([0,0,0]), np.array([0.9,0,-0.5])))
     # print(get_dictionary_angles(center, my_arr))
