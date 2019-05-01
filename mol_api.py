a70c9aaee2e6d79bba559d0867cfc10ae48c38fb
AnastasiiaMurzina <hloshkin@mail.ru>
2019-05-01 19:45:44 +0300

    Fix l_change

--- mol_api.py
+++ mol_api.py
@@ -23,6 +23,100 @@ class Notation():
             for i in range(len(item)):
                 self.bonds[key][i].insert(1, round(np.linalg.norm(self.atoms[key].position() - self.atoms[item[i][0]].position()), 1))
 
+    def diff(self, to_the_notation, b_diffs=False, s_diffs=False, l_diffs=False):
+        '''
+        TODO later it'd return the differents and check nums of atoms more correctly
+        :param to_the_notation: compare with this one notation
+        :param b_diffs: SOON
+        :param s_diffs: SOON
+        :param l_diffs: SOON
+        :return: int of different bonds, int of different section, sum of different bonds lengths
+        '''
+        if self.divider.n != to_the_notation.divider.n:
+            print("Different dividers!")
+            return
+        sections_diffs = 0
+        bonds_diffs = 0
+        length_diff = 0
+        for k_1, i_1 in self.notation.items():
+            k_2 = k_1
+            i_2 = to_the_notation.notation[k_2]
+            if len(i_1[0]) == len(i_2[0]):
+                for i, j in zip(i_1[0], i_2[0]):
+                    if i[0] != j[0]:
+                        bonds_diffs += 1
+                        print(k_1, i,j)
+                    elif i[1] != j[1]:
+                        sections_diffs += 1
+            else:
+                bonds_diffs += abs(len(i_2)-len(i_1))
+        for k, i in self.bonds.items():
+            if to_the_notation.bonds.get(k):
+                b2 = sorted(to_the_notation.bonds[k])
+            else:
+                continue
+            i = sorted(i)
+            if len(i) == len(b2):
+                for b01, b02 in zip(i, b2):
+                    if b01[0] == b02[0]:
+                        length_diff += abs(b02[1]-b01[1])
+            # else: calc smth else
+        return bonds_diffs, sections_diffs, length_diff
+
+    def s_diff(self, to_the_notation):
+        '''
+        :param to_the_notation: compare with this one notation
+        :return:
+        '''
+        if self.divider.n != to_the_notation.divider.n:
+            print("Different dividers!")
+            return
+        s_d = []
+        for k_1, i_1 in self.notation.items():
+            k_2 = k_1
+            i_2 = to_the_notation.notation[k_2]
+            if len(i_1[0]) == len(i_2[0]):
+                for inx, j in enumerate(zip(i_1[0], i_2[0])):
+                    i, j = j
+                    if i[0] == j[0] and i[1] != j[1]:
+                        s_d.append([k_1, inx, j[1]])
+        return s_d
+
+    def l_diff(self, to_the_notation):
+        '''
+        :param to_the_notation: compare with this one notation
+        :return:
+        '''
+        if self.divider.n != to_the_notation.divider.n:
+            print("Different dividers!")
+            return
+        # length_diff = 0
+        l_d = []
+        for k, i in self.bonds.items():
+            if to_the_notation.bonds.get(k):
+                b2 = sorted(to_the_notation.bonds[k])
+            else:
+                continue
+            i = sorted(i)
+            if len(i) == len(b2):
+                for inx, b01 in enumerate(zip(i, b2)):
+                    b01, b02 = b01
+                    if b01[0] == b02[0]:
+                        # length_diff += abs(b02[1]-b01[1])
+                        l_d.append([k, inx, b02])
+            # else: calc smth else
+        return l_d
+
+    def s_change(self, to_the_notation):
+        s_d = self.s_diff(to_the_notation)
+        for k_1, inx, j in s_d:
+            self.notation[k_1][0][inx][1] = j
+
+    def l_change(self, to_the_notation):
+        l_d = self.l_diff(to_the_notation)
+        for k_1, inx, j in l_d:
+            self.bonds[k_1][inx][1] = j[1]
+
 
 class Molecule():
     def __init__(self, mol2file='', n=5):
@@ -154,13 +248,16 @@ def searcher(substrat, product,zero_bonds_only=False,
     c1 = product.compare_with(np.array([i for _, i in dim_structure.items()]))
     paths = [c1]
     st = copy.deepcopy(substrat)
+    s_comp = substrat.notation.diff(product.notation)[1]
     while len(paths) < length_path:
     # while paths[-1] > 0.25:
 
         ch = st.get_child(zero=zero_bonds_only, change_length=length_change)
         ch_dim = dimensional_structure(ch.notation, relax=True)
         compared = pr.compare_with(np.array([i for _, i in ch_dim.items()]))
-        if 1-np.random.rand() < c1/compared:
+        _, s_comp_c, _ = st.notation.diff(pr.notation)
+        if s_comp_c < s_comp:
+        # if 1-np.random.rand() < c1/compared:
             paths.append(compared)
             print('structure is ', ch_dim)
             del st
@@ -172,15 +269,38 @@ if __name__ == '__main__':
     #           'reaction': 'mopac_example', #'3a->4'
     #           }
     n = 2
-    reaction = 'mopac_example' # '3a->4' #
+    # reaction = 'mopac_example' # '3a->4' #
+    reaction = '3a->4' #
     if reaction == '3a->4':
         ln = Molecule('./ordered_mol2/3a.mol2', n=n)
         pr = Molecule('./ordered_mol2/4_opted.mol2', n=n)
     else:
         ln = Molecule('./ordered_mol2/js_exapmle_init.mol2', n=n)
         pr = Molecule('./ordered_mol2/js_exapmle_finish.mol2', n=n)
-    path = searcher(ln, pr)
-    print(path)
+    # path = searcher(ln, pr)
+    # print(path)
+    # print(ln.notation.diff(pr.notation))
+    # print('reactant with orignal reactant')
+    # print(ln.compare_with(np.array([i for _, i in dimensional_structure(ln.notation).items()])))
+    print('before original pr with orignal reactant')
+    print(compare_structers(ln.to_positions_array(), pr.to_positions_array()))
+    # print('before original pr with dim_struct of reactant')
+    # print(pr.compare_with(np.array([i for _, i in dimensional_structure(ln.notation).items()])))
+    # print('before dim_structure of pr with dim_struct of reactant')
+    # print(compare_structers(np.array([i for _, i in dimensional_structure(ln.notation).items()]), np.array([i for _, i in dimensional_structure(pr.notation).items()])))
+    # print('product with product')
+    # print(pr.compare_with(np.array([i for _, i in dimensional_structure(pr.notation).items()])))
+    print(ln.notation.bonds)
+    ln.notation.l_change(pr.notation)
+    print(ln.notation.bonds)
+    print(dimensional_structure(ln.notation))
+
+    # print(compare_structers(np.array([i for _, i in dimensional_structure(ln.notation).items()]), np.array([i for _, i in dimensional_structure(pr.notation).items()])))
+    # print(pr.compare_with(np.array([i for _, i in dimensional_structure(ln.notation).items()])))
+    # print(pr.compare_with(np.array([i for _, i in dimensional_structure(ln.notation).items()])))
+    # print(ln.compare_with(pr.to_positions_array()))
+
+
     # print(ln.compare_with(pr.to_positions_array()))
     # print(pr.compare_with(np.array([i for _, i in dimensional_structure(ln.notation).items()])))
 
